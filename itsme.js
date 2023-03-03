5d604c3735a1cad0bd94999772752aa4e34e1b57
rtacode <rizkikotet31@gmail.com>
2023-03-03 22:37:45 +0700

    Update

---
+++ itsme.js
@@ -0,0 +1,6 @@
+const nama = "RizkiKotet";
+const asal = "Lebak, Banten";
+const keahlian = ["JavaScript", "HTML", "C++", "C#"];
+const umur = 19;
+const pacar = "Chichi";
+console.log(`Perkenalkan, nama saya ${nama}. Saya berasal dari ${asal}. Saya memiliki keahlian di bidang ${keahlian.join(", ")}.`);
---
+++ settings.js
@@ -0,0 +1,112 @@
+//Ubah Image/Thumbnail di folder maslent/pplent/maslent.png (Cari fotomu Ubah namnya ganti jadi maslent.png trus di gantu make pp mu yang baru
+
+const fs = require('fs')
+const chalk = require('chalk')
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+//==============================\
+autoread = true //jangan di ubah
+global.footer = 'RTA SERVER'//ganti
+global.pulsa = "6281287123512"//ganti
+global.gopay = "6281287123512"//ganti
+global.dana = "6281287123512"//ganti
+global.paypal = "---" // biarin aja kalau gaada
+global.shopay = "---" // biarin aja kalau gaada
+global.saweria = "---" // biarin aja kalau gaada
+global.sociabus = "---" // biarin aja kalau gaada
+global.bni = "---" // biarin aja kalau gaada
+global.bri = "---" // biarin aja kalau gaada
+global.bankjatim = "---" // biarin aja kalau gaada
+global.jago = "---" // biarin aja kalau gaada
+global.neobank = "---" // biarin aja kalau gaada
+global.packname = 'RTA SERVER'//ganti
+global.footer = '©RizkiKotet'//ganti
+global.ovo = "6281287123512"//ganti
+global.ownerlen = "RIZKI KOTET"//Ubah
+global.owner = ['6281287123512'] //ubh nomor owner hp
+global.ownername = "Rizki Kotet" //ubah
+global.ytname = "YT: rizkikotet"//ubah
+global.socialm = "IG: rizki_kotet"//ubah
+global.location = "Indonesia, Banten, Lebak"//ubah
+global.ownernomer = "6281287123512"//ubah nomorhp owner
+global.premium = ['6281287123512']//ubah nomor hp premium
+global.nomorcs = '6281287123512'
+global.botname = 'Bot V20' //ubah
+global.linkz = "Link Gc"//ubah
+global.websitex = "https://rizki-rta.xyz"//ubah
+global.botscript = '-'
+global.themeemoji = "🚀"//ubah
+global.packname = "Sticker By Rizkikotet"//ubah
+global.author = "Made by Rizkikotet"//ubah
+global.wm = "RizkiKotet"//ubah
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Gini?!', //ubah...
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only', 
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Upss Limit Kamu sudah habis..Tunggu Waktu menunjukan Pukul 12:00 Limit Anda otomatis akan Di reset!!!',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.thum = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.log0 = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.err4r = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.thumb = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+
+//SETTING AJA SUKA SUKA ASAL PERHATIIN TANDA PETIK MAUPUN KURUNG
+//Jagan lupa menunya di tambhin ya Soalnya saya capek wkkw
+global.allmenu = `
+•―――― ⌜ CONTOH 1⌟ ――――•
+bla bla
+bla bla
+•―――― ⌜ CONTOH 1⌟ ――――•
+bla bla
+bla bla
+•―――― ⌜ CONTOH 1⌟ ――――•
+
+//dan text yang di awali // jangan di hapus sebagai penanda saja
+//ada petik di bawah text ini jangan di hapus kalau mau nambah list langsung tambah aja jangan hapus text ini dan petik di bawa ini
+`
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
